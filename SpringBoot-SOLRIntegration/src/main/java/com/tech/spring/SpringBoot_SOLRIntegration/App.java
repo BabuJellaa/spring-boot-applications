53abdb3afbb5b0e6de901678ecf21ae8c1744e76
babu j <babu.j@techouts.com>
2024-12-24 13:30:03 +0530

    Solr integration

---
+++ SpringBoot-SOLRIntegration/src/main/java/com/tech/spring/SpringBoot_SOLRIntegration/App.java
@@ -0,0 +1,13 @@
+package com.tech.spring.SpringBoot_SOLRIntegration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
